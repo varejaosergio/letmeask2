9750ec0e9548f0c592904545a3b84112a4f9c8e1
Sérgio Schwartz Varejão <43224989+varejaosergio@users.noreply.github.com>
2021-06-23 12:45:44 -0700

    first commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,75 @@
+import { FormEvent, useState } from 'react';
+import { useHistory } from 'react-router-dom'
+
+import illustrationImg from '../assets/illustration.svg';
+import logo from '../assets/logo.svg';
+import googleIconImg from '../assets/google-icon.svg';
+
+import { Button } from '../components/Button'
+
+import '../styles/auth.scss'
+import { useAuth } from '../hooks/useAuth';
+import { database } from '../services/firebase';
+
+export function Home() {
+    const history = useHistory();
+    const { user, signInWithGoogle } = useAuth();
+    const [roomCode, setRoomCode] = useState('');
+
+    async function handleCreateRoom() {
+        if (!user) {
+            await signInWithGoogle();
+        }
+
+        history.push('/rooms/new');
+    }
+
+    async function handleJoinRoom(event: FormEvent) {
+        event.preventDefault();
+
+        if(roomCode.trim() === '') {
+            return;
+        }
+
+        const roomRef = await database.ref(`rooms/${roomCode}`).get();
+
+        if(!roomRef.exists()) {
+            alert('Room does not exists.');
+
+            return;
+        }
+
+        history.push(`/rooms/${roomCode}`);
+    }
+
+    return (
+        <div id="page-auth">
+            <aside>
+                <img src={illustrationImg} alt='illustration' />
+                <strong>Crie salas de Q&amp;A ao vivo</strong>
+                <p>Tire as duvidas da audiencia em tempo real</p>
+            </aside>
+            <main>
+                <div className="main-content">
+                    <img src={logo} alt="letmeask" />
+                    <button onClick={handleCreateRoom} className="create-room">
+                        <img src={googleIconImg} alt='logo do google' />
+                        Crie sua sala com o Google
+                    </button>
+                    <div className="separator">Ou entre em uma sala</div>
+                    <form onSubmit={handleJoinRoom}>
+                        <input
+                            type='text'
+                            placeholder='Digite o codigo da sala'
+                            onChange={event => setRoomCode(event.target.value)}
+                            value={roomCode}
+                        />
+                        <Button type='submit'>
+                            Entrar na sala
+                        </Button>
+                    </form>
+                </div>
+            </main>
+        </div>
+    );
+}
---
+++ src/pages/Room.tsx
@@ -0,0 +1,32 @@
+import logoimg from '../assets/logo.svg';
+import { Button } from '../components/Button';
+
+import '../styles/room.scss'
+
+export function Room() {
+    return(
+        <div id="page-room">
+            <header>
+                <div className="content">
+                    <img src={logoimg} alt="Letmeask"/>
+                    <div>codigo</div>
+                </div>
+            </header>
+            <main className="content">
+                <div className="room-tittle">
+                    <h1>Sala React</h1>
+                    <span>4 perguntas</span>
+                </div>
+
+                <form>
+                    <textarea 
+                        placeholder="O que você quer perguntar" 
+                    />
+                    <div className="form-footer">
+                        <span>Para enviar uma pergunta, <Button>faça seu login</Button></span>
+                    </div>
+                </form>
+            </main>
+        </div>
+    );
+}
